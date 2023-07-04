d87988c640d8e4bf34dd5badb2ed9175add92632
kotaro <big.i.land.k@gmail.com>
2023-07-04 13:51:13 +0900

    modified prompt

--- pdf_loader.py
+++ pdf_loader.py
@@ -1,5 +1,6 @@
-from langchain.document_loaders import PyPDFLoader
-
-loader = PyPDFLoader("doc_class.pdf")
-pages = loader.load_and_split()
-print(pages)
+from langchain.document_loaders import PDFMinerPDFasHTMLLoader
+loader = PDFMinerPDFasHTMLLoader("doc_class.pdf")
+data = loader.load()[0]   # entire pdf is loaded as a single Document
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(data.page_content,'html.parser')
+content = soup.find_all('div')
